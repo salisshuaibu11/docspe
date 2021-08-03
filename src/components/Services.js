975ce81473bd882e5203bd930e352e04942c529e
salis <salisshuaibu11@gmail.com>
2021-08-03 10:31:42 +0000

    Added Services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,19 +1,74 @@
 import React from "react";
 import {
   Heading,
-  Box
- } from "@chakra-ui/react";
+  Box,
+  Text,
+  Container,
+  Stack,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 import ButtonComponent from "./UI/Button";
 
 const Services = () => {
   return (
-    <div>
+    <Stack bg="gray.100" py="3rem">
       <Heading as="h3" size="lg" align="center">
         Our Services
       </Heading>
-    </div>
-  )
-}
+      <SimpleGrid align="center" minChildWidth="120px" spacing="40px" px="10px">
+        <Box p={5}>
+          <Container w={20} rounded="full" bg="white" p={5}>
+            <PhoneIcon color="blue.500" />
+          </Container>
+          <Heading fontSize="1.3rem" my="2rem" weight="bold" as="h2">
+            MyChron
+          </Heading>
+          <Text weight="100" font="normal">
+            Access video consultation with Malaysia's top doctors on the Practo
+            app.
+          </Text>
+        </Box>
+        <Box p={5}>
+          <Container w={20} rounded="full" bg="white" p={5}>
+            <PhoneIcon color="blue.500" />
+          </Container>
+          <Heading fontSize="1.3rem" my="2rem" weight="bold" as="h2">
+            Virtual Visit
+          </Heading>
+          <Text weight="100" font="normal">
+            Access video consultation with Malaysia's top doctors on the Practo
+            app.
+          </Text>
+        </Box>
+        <Box p={5}>
+          <Container w={20} rounded="full" bg="white" p={5}>
+            <PhoneIcon color="blue.500" />
+          </Container>
+          <Heading fontSize="1.3rem" my="2rem" weight="bold" as="h2">
+            COVID Center
+          </Heading>
+          <Text weight="100" font="normal">
+            Access video consultation with Malaysia's top doctors on the Practo
+            app.
+          </Text>
+        </Box>
+        <Box p={5}>
+          <Container w={20} rounded="full" bg="white" p={5}>
+            <PhoneIcon color="blue.500" />
+          </Container>
+          <Heading fontSize="1.3rem" my="2rem" weight="bold" as="h2">
+            Pharmacy
+          </Heading>
+          <Text weight="100" font="normal">
+            Access video consultation with Malaysia's top doctors on the Practo
+            app.
+          </Text>
+        </Box>
+      </SimpleGrid>
+    </Stack>
+  );
+};
 
 export default Services;
